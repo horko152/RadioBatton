a672e0b9f24ed96c0a1f205d5789e2bc077f8e19
Igor Voloshenyuk <voloshenyuk98@gmail.com>
2020-02-25 16:06:12 +0200

    Added migration

--- DAL/Migrations/20200225140329_Added_role.Designer.cs
+++ DAL/Migrations/20200225140329_Added_role.Designer.cs
@@ -4,14 +4,16 @@ using DAL;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DAL.Migrations
 {
     [DbContext(typeof(RadioBattonDbContext))]
-    partial class RadioBattonDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200225140329_Added_role")]
+    partial class Added_role
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
